b5663320aad6309d972fc3268c6c8e901e6f06b2
Przemek <przemek8131@gmail.com>
2025-05-05 17:27:34 +0200

    Change vehicle ID type

--- cross.c
+++ cross.c
@@ -1,4 +1,6 @@
-#include"cross.h"
+#include "cross.h"
+#include <string.h>
+#include <stdlib.h>
 //#include<stdio.h>
 
 
@@ -50,11 +52,14 @@ static turn_t which_turn(road_t* road, direction_t end_direction) {
 	}
 	return turn;
 }
-void add_vehicle(road_t* roads, uint8_t ID, direction_t start_direction, direction_t end_direction) {
+void add_vehicle(road_t* roads, const char* ID, direction_t start_direction, direction_t end_direction) {
 	
 	for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
 		if (roads[i].direction == start_direction) {
-			roads[i].vehicles[roads[i].car_num].ID = ID;
+			roads[i].vehicles[roads[i].car_num].ID = malloc(strlen(ID)+1);
+			if(roads[i].vehicles[roads[i].car_num].ID != NULL)
+#pragma warning(disable:4996) // wy³¹cza ostrze¿enie o strcpy
+				strcpy(roads[i].vehicles[roads[i].car_num].ID, ID);
 			roads[i].vehicles[roads[i].car_num].end_direction = end_direction;
 			roads[i].vehicles[roads[i].car_num].turn = which_turn(&roads[i], end_direction);
 			roads[i].car_num++;
@@ -63,9 +68,9 @@ void add_vehicle(road_t* roads, uint8_t ID, direction_t start_direction, directi
 	}
 }
 
-//TODO: zwraca id, zmiana na nazwe
-static uint8_t left_vehicle(road_t* road) {
-	uint8_t ID = 0;
+//TODO: zwraca wskaŸnik na dynamicznie zaalokowan¹ pamiêæ z ID pojazdu, nale¿y pamiêtaæ o jej zwolnieniu
+static char* left_vehicle(road_t* road) {
+	char* ID = NULL;
 	if (road->light_state == GREEN_LIGHT) {
 		if (road->car_num != 0) {
 			ID = road->vehicles[0].ID;
@@ -76,13 +81,11 @@ static uint8_t left_vehicle(road_t* road) {
 			//
 			road->car_num--;
 		}
-		else
-			return 0;
 	}
 	return ID;
 }
 //TODO: powinna zwracaæ step_count
-void cross_update(road_t* roads, uint8_t* buff) {
+void cross_update(road_t* roads, char** buff) {
 	static uint8_t step_count = 0; // TODO: do wypisania pojazdów w danym kroku trzeba bêdzie zmieniæ bufor w strukture z danymi i z krokiem 
 	for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
 		buff[i] = left_vehicle(&roads[i]);
@@ -94,30 +97,41 @@ void change_lights(road_t* road, light_state_t light_state) {
 	road->light_state = light_state;
 }
 
-void cross_step(road_t* roads, uint8_t* buff) {
+void cross_step(road_t* roads, char** buff) {
 	cross_update(roads, buff);
 	direction_t direction = busiest_direction(roads);
 	direction_t opposite_direction = roads[direction].vehicles[0].end_direction;
-	if ((roads[direction].vehicles[0].turn == straight || roads[direction].vehicles[0].turn == right) && (opposite_direction == straight || opposite_direction == right)) {
-		for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
-			if (roads[i].direction == direction || roads[i].direction == opposite_direction)
-				change_lights(&roads[i], GREEN_LIGHT);
-			else
-				change_lights(&roads[i], RED_LIGHT);
+	if (roads[opposite_direction].car_num > 0) {
+		if ((roads[direction].vehicles[0].turn == straight || roads[direction].vehicles[0].turn == right) && (roads[opposite_direction].vehicles[0].turn == straight || roads[opposite_direction].vehicles[0].turn == right)) {
+			for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
+				if (roads[i].direction == direction || roads[i].direction == opposite_direction)
+					change_lights(&roads[i], GREEN_LIGHT);
+				else
+					change_lights(&roads[i], RED_LIGHT);
+			}
+
+			//change_lights(roads, direction, GREEN_LIGHT);
+			//change_lights(roads, roads[direction].vehicles[0].end_direction, GREEN_LIGHT);
 		}
-		
-		//change_lights(roads, direction, GREEN_LIGHT);
-		//change_lights(roads, roads[direction].vehicles[0].end_direction, GREEN_LIGHT);
+		//else if()
+		else {
+			for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
+				if (roads[i].direction == direction)
+					change_lights(&roads[i], GREEN_LIGHT);
+				else
+					change_lights(&roads[i], RED_LIGHT);
+			}
+
+		}
 	}
-	//else if()
 	else {
+		//tu mozna wybraæ wtedy inny zamiast tego opposite
 		for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
-			if(roads[i].direction == direction)
+			if (roads[i].direction == direction)
 				change_lights(&roads[i], GREEN_LIGHT);
 			else
 				change_lights(&roads[i], RED_LIGHT);
 		}
-		
 	}
 	//roads[direction] dziala tak bez pêtli dopóki s¹ cztery drogi i wpisane s¹ w tej samej kolejnoœci jako enum i do tablicy roads
 	//cross_update(roads, buff);
@@ -149,4 +163,16 @@ char which_light(light_state_t light) {
 	case YELLOW_LIGHT:
 		return 'Y';
 	}
+}
+//funkcja do przekazania wy¿ej jakie pojazdy odjecha³y// zwalnianie pamiêci !!!
+void print_left(char** left_vehicle_buff) {
+	printf("-----------------------------------------------------\n");
+	for (uint8_t i = 0; i < NUM_OF_ROADS; i++) {
+		if (left_vehicle_buff[i] != 0) {
+			printf("Opuscil: %s z kierunku: %c\n", left_vehicle_buff[i], which_direction(i));
+			free(left_vehicle_buff[i]);
+		}
+	}
+	memset(left_vehicle_buff, 0, sizeof(left_vehicle_buff));
+	printf("-----------------------------------------------------\n");
 }
--- cross.h
+++ cross.h
@@ -6,6 +6,7 @@
 #include<stdbool.h>
 
 #define VEHICLES_SIZE (10)
+#define VEHICLES_ID_SIZE (10) //Vehicle1 - Vehicle99 + \0
 #define NUM_OF_ROADS (4)
 //#define NUM_OF_LINES (1)
 //TODO: powinien byæ jeszcze RED_YELLOW_LIGHT, jako ¿ó³te po czerwonym
@@ -28,7 +29,8 @@ typedef enum {straight,
 } turn_t;
 
 typedef struct {
-	uint8_t ID;			//TODO: trzeba zmienic na string
+	char* ID;
+	//TODO: trzeba zmienic na string
 	//direction_t startRoad;
 	direction_t end_direction;
 	turn_t turn;
@@ -64,13 +66,14 @@ typedef struct {
 //} road_t;
 
 void road_initialize(road_t* road, direction_t direction);
-void add_vehicle(road_t* roads, uint8_t ID, direction_t startRoad, direction_t endRoad);
+void add_vehicle(road_t* roads, const char* ID, direction_t startRoad, direction_t endRoad);
 //void change_lights(road_t* road, direction_t direction, light_state_t light_state);
 void change_lights(road_t* road, light_state_t light_state);
-void cross_update(road_t* roads, uint8_t* buff);
+void cross_update(road_t* roads, char** buff);
 
 
 ////////////////testowe
 char which_direction(direction_t direction);
 char which_light(light_state_t light);
+void print_left(uint8_t* left_vehicle_buff);
 #endif
